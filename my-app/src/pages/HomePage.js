abe49f1efc26bf0c467dd2c4b071d3911e306547
benson-dylan <143048807+benson-dylan@users.noreply.github.com>
2025-03-13 15:44:16 -0400

    Gamers

--- my-app/src/pages/HomePage.js
+++ my-app/src/pages/HomePage.js
@@ -6,7 +6,10 @@ function HomePage()
     return (
         <div class="HomePage">
             <div id="HomeTop">
-                <h1>Welcome friends</h1>
+                <h1>Welcome gamers</h1>
+            </div>
+            <div id="HomeTwo">
+                <h1>Get ready to game...</h1>
             </div>
         </div>
     );
