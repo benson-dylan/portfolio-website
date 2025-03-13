4661fdde4aa4c5d9b8c7329d95c4df23a9ad41ad
benson-dylan <143048807+benson-dylan@users.noreply.github.com>
2025-03-13 14:42:24 -0400

    Add router dom

---
+++ my-app/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/index.css';
+
+function HomePage()
+{
+    return(
+        <div>
+            <h1>hello there</h1>
+        </div>
+    );
+}
+
+export default HomePage;
