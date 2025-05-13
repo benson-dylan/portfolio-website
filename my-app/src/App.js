6eb2ff0e651c5e262bb23cbff72b24530feb1477
benson-dylan <143048807+benson-dylan@users.noreply.github.com>
2025-05-12 23:56:12 -0400

    Fixed index stuff

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,19 +6,11 @@ import HomePage from './pages/HomePage';
 import ProjectPage from './pages/Projects';
 import ELLEvation from './pages/ELLEvation';
 import AboutPage from './pages/AboutMe';
-import fish from './images/fish.png';
 
 const prefix = "/portfolio-website";
 
 function App() {
   return (
-      <html>
-        <head>
-          <link rel="icon" type="image/png" size="192x192" href={fish}/>
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-          <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet"></link>
-        </head>
         <BrowserRouter>
           <Routes>
             <Route path={prefix + "/"} element={<HomePage/>}/>
@@ -27,7 +19,6 @@ function App() {
             <Route path={prefix + "/about"} element={<AboutPage/>}/>
           </Routes>
         </BrowserRouter>
-      </html>
   );
 }
 
